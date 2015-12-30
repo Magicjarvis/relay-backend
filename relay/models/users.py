a00b245728f6266b6ea6e9af4c25f2bbfb1836b5
Jarvis Johnson <magicjarvis@gmail.com>
2015-12-30 13:37:48 -0800

    tiny fixes, add display names

--- relay/models/users.py
+++ relay/models/users.py
@@ -15,21 +15,26 @@ def get_usernames():
 
 
 def add_user(username, password, email, gcm_id=None, session_token=None):
+  display_name = username
   username = sanitize_username(username)
   gcm_ids = [gcm_id] if gcm_id else []
   hashed_password = hash_password(password)
   new_user = User(
     id=username,
+    display_name=display_name,
     password=hashed_password,
     email=email,
     gcm_ids=gcm_ids,
-    session_token=session_token
+    session_tokens=[session_token]
   ).put()
   return new_user is not None
 
 
 class User(ndb.Model):
   """Models a user."""
+  # we prefer showing this name, regular username is key
+  display_name = ndb.StringProperty()
+
   password = ndb.StringProperty(required=True)
   email = ndb.StringProperty()
   session_tokens = ndb.StringProperty(indexed=True, repeated=True)
